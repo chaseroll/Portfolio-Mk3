5a17df49decb0431e3ac1a69a3961a09a047196f
ShafiHabibi <shafihabibiwork@gmail.com>
2025-02-19 10:46:29 -0600

    feat: add transparent glass effect on image reveal.

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -1,9 +1,9 @@
 import profileImgPlaceholder from 'assets/profile-placeholder.jpg';
 import profileKatakana from 'assets/katakana-profile.svg?url';
 import profileImg1 from 'assets/profile-1.jpg';
-import Signature from 'assets/signature.png';
-import SignaturePlaceholder from 'assets/signaturePlaceholder.png';
-import profileImg2 from 'assets/profile-2.jpg';
+// import Signature from 'assets/signature.png';
+// import SignaturePlaceholder from 'assets/signaturePlaceholder.png';
+// import profileImg2 from 'assets/profile-2.jpg';
 import { Button } from 'components/Button';
 import { DecoderText } from 'components/DecoderText';
 import { Divider } from 'components/Divider';
@@ -16,7 +16,7 @@ import { Transition } from 'components/Transition';
 import { Fragment, useState } from 'react';
 import { media } from 'utils/style';
 import styles from './Profile.module.css';
-import TiltPhaseSix from 'components/Image/tiltPhaseSix';
+// import TiltPhaseSix from 'components/Image/tiltPhaseSix';
 
 const options = {
   max: 10,
@@ -37,40 +37,47 @@ const ProfileText = ({ visible, titleId }) => (
       as="p"
       style={{ color: '#808080' }}
     >
-      {"I'm "}
+      {'My name is '}
       <Link href="/resume">Chase Roll</Link>
-      {`, a grade 12 high school student passionate about learning and improvement. Driven by an insatiable curiosity and inspired by innovators like Elon Musk,`}
-      <Link href="">{` I've combined my passions for learning and building`}</Link>
-      {` to develop a myriad of projects and launch various entrepreneurial ventures.`}
+      {`, I'm 18, and I want to change the world. History’s greatest innovators—`}
+      <Link href="">{` Einstein, Jobs, Musk`}</Link>
+      {` —were not products of a traditional, rigid academic system; they were individuals who carved their own `}
+      <Link href="">{`intellectual`}</Link>
+      {` paths, pursued their ideas relentlessly, and built the future with their own hands.`}
       <br />
       <br />
-      {`In each of these projects,`}
-      <Link href="">{` I have aimed not just to build something cool but also to understand the principles behind them.`}</Link>
-      {` This duality has been a cardinal theme in my life, introducing me to fields of study I may not have otherwise explored.`}
+      {`I intend to do the same. I have realized that this is what I love:`}
+      <Link href="">{`  learning, building, creating`}</Link>
+      {` —starting with a raw concept, molding it into reality, and navigating the risks, challenges, and setbacks that arise along the way.`}
       <br />
       <br />
-      {`As I near the end of my high school years, I'm preparing myself for university.`}
-      <Link href="">{` I plan to major in physics and pursue a dual minor in electrical engineering and computer science.`}</Link>
+      {'My '}
+      <Link href="">{`ambition`}</Link>
+      {` is to channel this `}
+      <Link href="">{`indefatigable energy`}</Link>
+      {` toward the pursuit of truth and the `}
+      <Link href="">{`creation of systems, institutions,  and technologies`} </Link>
+      {` that fundamentally upend the status quo and dramatically improve the human condition. In essence, I aim to shape the world toward the good.`}
+      {/* <Link href="">{` I plan to major in physics and pursue a dual minor in electrical engineering and computer science.`}</Link>
       <br />
       <br />
       {`Aware of life's fleeting nature, `}
       <Link href="">{`I strive to make the most of every second I have on this planet.`}</Link>
-      {` My ultimate ambition is to become the best possible version of myself and to spearhead impactful technologies that improve the human condition.`}
+      {` My ultimate ambition is to become the best possible version of myself and to spearhead impactful technologies that improve the human condition.`} */}
     </Text>
 
-    {/* <div className = {styles.signature}>
+    {/* <div className={styles.signature}>
       <Image
         delay={0}
         placeholder={SignaturePlaceholder}
         srcSet={[Signature]}
         sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
         alt="Me standing in front of the Torii on Miyajima, an island off the coast of CA in USA"
-        style={{ position: "absolute", top: "100", left: "0", right: "0", bottom: "0"}}
+        style={{ position: 'absolute', top: '100', left: '0', right: '0', bottom: '0' }}
       />
     </div> */}
   </Fragment>
 );
-
 
 export const Profile = ({ id, visible, sectionRef }) => {
   const [focused, setFocused] = useState(false);
@@ -92,10 +99,12 @@ export const Profile = ({ id, visible, sectionRef }) => {
           <div className={styles.content}>
             <div className={styles.column}>
               <ProfileText visible={visible} titleId={titleId} />
-              <div style={{
-                display: "flex",
-                gap: "30px"
-              }}>
+              <div
+                style={{
+                  display: 'flex',
+                  gap: '30px',
+                }}
+              >
                 <Button
                   secondary
                   className={styles.button}
@@ -110,26 +119,30 @@ export const Profile = ({ id, visible, sectionRef }) => {
                   className={styles.button}
                   data-visible={visible}
                   href="/resume"
-                  icon='resume'
+                  icon="resume"
                 >
                   My Resume
                 </Button>
               </div>
             </div>
-            <div className={styles.column} style={{ marginTop: "-100px" }}>
+            <div className={styles.column} style={{ marginTop: '-100px' }}>
               <div className={styles.tag} aria-hidden>
                 <Divider
                   notchWidth="64px"
                   notchHeight="8px"
                   collapsed={!visible}
                   collapseDelay={2000}
-
                 />
                 <div className={styles.tagText} data-visible={visible}>
                   About Me
                 </div>
               </div>
-              <div className={styles.image} style={{ borderRadius: '100px !important' }}>
+              <div
+                className={styles.image}
+                style={{
+                  borderRadius: '100px !important',
+                }}
+              >
                 {/* <Image
                   noPauseButton
                   play={true}
@@ -152,7 +165,7 @@ export const Profile = ({ id, visible, sectionRef }) => {
                   delay={100}
                   placeholder={profileImgPlaceholder}
                   srcSet={[profileImg1, profileImg1]}
-                  style={{clipPath: 'polygon(0 0, 100% 0, 100% 85%, 85% 100%, 0 100%)'}}
+                  style={{ clipPath: 'polygon(0 0, 100% 0, 100% 85%, 85% 100%, 0 100%)' }}
                   sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
                   alt="Me at Gibb's Garden"
                   className={styles.imageZoom}
@@ -174,8 +187,8 @@ export const Profile = ({ id, visible, sectionRef }) => {
                   className={styles.svg}
                   data-visible={visible}
                   style={{
-                    position: "absolute",
-                    top: "0px"
+                    position: 'absolute',
+                    top: '0px',
                   }}
                 >
                   <use href={`${profileKatakana}#katakana-profile`} />
